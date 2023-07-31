65c05194937ae681a519ae50526787133eaa9560
YesCoach <sbsii1020@gmail.com>
2023-07-31 16:40:21 +0900

    feat: Model 타입 구현 및 Data 추가

---
+++ BookShelf/Model/Movie.swift
@@ -0,0 +1,18 @@
+//
+//  Movie.swift
+//  BookShelf
+//
+//  Created by 박태현 on 2023/07/31.
+//
+
+import Foundation
+
+struct Movie {
+    var title: String
+    var releaseDate: String
+    var isFavorite: Bool = false
+    var runtime: Int
+    var overview: String
+    var rate: Double
+    var posterImageName: String
+}
